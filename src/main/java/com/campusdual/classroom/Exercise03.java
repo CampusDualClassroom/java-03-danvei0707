623082934ca83a75ef69928f28db0987fe412158
Daniel <dani.v2509@outlook.com>
2024-10-10 15:30:29 +0200

    Solution

--- src/main/java/com/campusdual/classroom/Exercise03.java
+++ src/main/java/com/campusdual/classroom/Exercise03.java
@@ -20,13 +20,13 @@ public class Exercise03 {
         if ((user == "Sebas") && (pass == "sebas01")) {
             System.out.println(greetings(user));
         } else {
-            System.out.println(error(user));
+            System.out.println(error("Sebas"));
         }
     }
 
 
     public static void main(String[] args) {
         checkUser("Sebas", "sebas01");
-        checkUser("Sebas", "olivaExtra77");
+        checkUser("Jorge", "olivaExtra77");
     }
 }
